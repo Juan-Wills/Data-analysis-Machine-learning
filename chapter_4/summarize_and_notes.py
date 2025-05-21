039cfb067cbd890b0ce5c0bef39ac80431c5f811
Juan-Wills <juandavidwills@gmail.com>
2025-05-21 11:50:06 -0500

    Updating structure, first comments

---
+++ chapter_4/summarize_and_notes.py
@@ -0,0 +1,29 @@
+"""
+Numpy (Numerical Python) is used for better numerical computing in python.
+
+Some tools numpy provide:
+    1. Array-oriented arithmetic operations with broadcasting capabilities
+    2. Multidimentional arrays
+    3. Eficient and fast mathematical functions
+    4. Reading/writting array data to disk
+    5. Linear algebra, random number generator, and Fourier
+       transform capabilities
+    6. C API for connecting Numpy with libraries written in C,
+       C++, or FORTRAN
+
+Applications in data analysis:
+   1.
+   2.
+   3.
+   4.
+   5.
+"""
+
+# 4.1-Ndarray_a_Multidimentional_Array_Object
+# 4.2-Pseudorandom_Number_Generator
+# 4.3-Universal_Functions_Fast_Element-Wise_Array_Functions
+# 4.4-Array-Oriented_Programming_with_Arrays
+# 4.5-File_Input_and_Output_with_Arrays
+# 4.6-Linear_Algebra
+# 4.7-Example-_Random_Walks
+# 4.8-Conclusion
